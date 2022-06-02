2e59104b187794fac95b1163f96a6e272a571f56
Juan Solis <Juansa@estudiantec.cr>
2022-06-02 00:37:18 -0600

    A

--- EstimacionParametros.py
+++ EstimacionParametros.py
@@ -3,50 +3,55 @@ import pandas
 import scipy.stats as stats
 import matplotlib.pyplot as plt
 import math
+from sympy import re
 
 datosExcel = pandas.read_excel("Tarea3\Conjunto_datos_tarea2.xlsx")
 
-
-
 #Se separan en diferentes listas las muestras utilizadas
-muestraInicial = datosExcel['Inicial']
-muestraPrimerCambio=datosExcel["Primer_cambio"]
-muestraSegundoCambio=datosExcel["Segundo_cambio"]
+def extracion_de_datos():
+    muestraInicial = datosExcel['Inicial']
+    muestraPrimerCambio=datosExcel["Primer_cambio"]
+    muestraSegundoCambio=datosExcel["Segundo_cambio"]
+    return (muestraInicial, muestraPrimerCambio, muestraSegundoCambio)
 
 #se calcula la media para la muestra incial, muestra con el Primer Cambio y Muestras con el segundo cambio 
-mediaIncial = muestraInicial.mean()
-mediaCambio1 = muestraPrimerCambio.mean()
-mediaCambio2 = muestraSegundoCambio.mean()
+def calculo_de_medias():
+    mediaIncial = extracion_de_datos()[0].mean()
+    mediaCambio1 = extracion_de_datos()[1].mean()
+    mediaCambio2 = extracion_de_datos()[2].mean()
+    return (mediaIncial, mediaCambio1, mediaCambio2)
 
 #Calculo de las desviaciones estandar para la muestra incial, muestra con el Primer Cambio y Muestras con el segundo cambio 
-desviacionEstandarInciail = muestraInicial.std()
-desviacionEstandarCambio1 = muestraPrimerCambio.std()
-desviacionEstandarCambio2 = muestraSegundoCambio.std()
-
-
+def calculo_de_desviaciones():
+    desviacionEstandarInciail = extracion_de_datos()[0].std()
+    desviacionEstandarCambio1 = extracion_de_datos()[1].std()
+    desviacionEstandarCambio2 = extracion_de_datos()[2].std()
+    return (desviacionEstandarInciail, desviacionEstandarCambio1 , desviacionEstandarCambio2)
 
 #se calcula la desviacion estadar de las muestra inical, la muestra del primer cambio y la muestras del segundo cambio con MLE usando la media conocida
-desviacionEstandarInciailMLE= stats.norm.fit(muestraInicial,floc=mediaIncial)[1]
-desviacionEstandarCambio1MLE= stats.norm.fit(muestraPrimerCambio,floc=mediaCambio1)[1]
-desviacionEstandarCambio2MLE= stats.norm.fit(muestraSegundoCambio,floc=mediaCambio2)[1]
+def calculo_desviacion_con_MLE():
+    desviacionEstandarInciailMLE= stats.norm.fit(extracion_de_datos()[0],floc=calculo_de_medias()[0])[1]
+    desviacionEstandarCambio1MLE= stats.norm.fit(extracion_de_datos()[1],floc=calculo_de_medias()[1])[1]
+    desviacionEstandarCambio2MLE= stats.norm.fit(extracion_de_datos()[2],floc=calculo_de_medias()[2])[1]
+    return (desviacionEstandarInciailMLE, desviacionEstandarCambio1MLE, desviacionEstandarCambio2MLE)
 
-print("media inicial: ", mediaIncial)
-print("desviacion estandar inicial: ", desviacionEstandarInciail)
-print("desviacion estandar inicial MLE: ", desviacionEstandarInciailMLE)
-print("varianza inicial: ", math.pow(desviacionEstandarInciail,2))
-print("varianza inicial MLE: ", math.pow(desviacionEstandarInciailMLE,2))
-print("---------------------------------------------------------------------------------------------------------")
+print("media inicial: ", calculo_de_medias()[0])
+print("desviacion estandar inicial: ", calculo_de_desviaciones()[0])
+print("desviacion estandar inicial MLE: ", calculo_desviacion_con_MLE()[0])
+print("varianza inicial: ", math.pow(calculo_de_desviaciones()[0],2))
+print("varianza inicial MLE: ", math.pow(calculo_desviacion_con_MLE()[0],2))
+print("#########################################################################################################")
 
-print("media primer cambio: ", mediaCambio1)
-print("desviacion estandar primer cambio: ", desviacionEstandarCambio1)
-print("desviacion estandar primer cambio MLE: ", desviacionEstandarCambio1MLE)
-print("varianza primer cambio: ", math.pow(desviacionEstandarCambio1,2))
-print("varianza primer cambio MLE: ", math.pow(desviacionEstandarCambio1MLE,2))
-print("---------------------------------------------------------------------------------------------------------")
+print("media primer cambio: ", calculo_de_medias()[1])
+print("desviacion estandar primer cambio: ", calculo_de_desviaciones()[1])
+print("desviacion estandar primer cambio MLE: ", calculo_desviacion_con_MLE()[1])
+print("varianza primer cambio: ", math.pow(calculo_de_desviaciones()[1],2))
+print("varianza primer cambio MLE: ",math.pow(calculo_desviacion_con_MLE()[1],2))
+print("#########################################################################################################")
 
-print("media segundo cambio: ", mediaCambio2)
-print("desviacion estandar segundo cambio: ", desviacionEstandarCambio2)
-print("desviacion estandar segundo cambio MLE: ", desviacionEstandarCambio2MLE)
-print("varianza segundo cambio: ", math.pow(desviacionEstandarCambio2,2))
-print("varianza segundo cambio MLE: ", math.pow(desviacionEstandarCambio2MLE,2))
-print("---------------------------------------------------------------------------------------------------------")
+print("media segundo cambio: ", calculo_de_medias()[2])
+print("desviacion estandar segundo cambio: ",calculo_de_desviaciones()[2])
+print("desviacion estandar segundo cambio MLE: ", calculo_desviacion_con_MLE()[2])
+print("varianza segundo cambio: ", math.pow(calculo_de_desviaciones()[2],2))
+print("varianza segundo cambio MLE: ", math.pow(calculo_desviacion_con_MLE()[2],2))
+print("#########################################################################################################")
